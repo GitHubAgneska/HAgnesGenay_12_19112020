9d74377d0977c1b14c456126e2e5033a2f38801d
Agneska <40915591+GitHubAgneska@users.noreply.github.com>
2021-09-03 06:29:43 +0200

    barcharts component draft

--- src/components/elements/UserActivity.jsx
+++ src/components/elements/UserActivity.jsx
@@ -1,6 +1,6 @@
 import PropTypes from "prop-types"
 import {Fragment} from "react"
-import {BarChart, CartesianGrid, ReferenceLine, XAxis, Tooltip, Legend, Bar, ResponsiveContainer} from "recharts"
+import {BarChart, ReferenceLine, XAxis, YAxis,Tooltip, Legend, Bar, ResponsiveContainer} from "recharts"
 
 const UserActivity = ({userActivitySessions}) => {
 
@@ -11,8 +11,9 @@ const UserActivity = ({userActivitySessions}) => {
 
                     <BarChart width={730} height={250} barSize={5} data={userActivitySessions}>
 
-                        <XAxis dataKey="day"/>
-                        <ReferenceLine y={100} label="" stroke="grey" strokeDasharray="3 3" />
+                        <XAxis />
+                        <YAxis orientation="right" tickCount="3" />
+{/*  <YAxis type= "category" allowDuplicatedCategory="true" dataKey="kilogram"  dataKey="calories" orientation="right" tickCount="3"  allowDecimals="false"/>*/}                        <ReferenceLine y={100} label="" stroke="grey" strokeDasharray="3 3" />
                         <ReferenceLine y={250} stroke="grey" strokeDasharray="3 3" />
                         <Legend/>
                         <Bar dataKey="kilogram" fill="#8884d8"/>
