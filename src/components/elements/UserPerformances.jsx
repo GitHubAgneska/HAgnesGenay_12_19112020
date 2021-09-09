2f5ee36f57b9281b9b9b37c54ec291a6a9df396b
Agneska <40915591+GitHubAgneska@users.noreply.github.com>
2021-09-09 12:05:27 +0200

    style : wrapping-up chart3 (radarChart) customization

--- src/components/elements/UserPerformances.jsx
+++ src/components/elements/UserPerformances.jsx
@@ -4,8 +4,8 @@ import styled from "styled-components"
 
 const Wrapper = styled.section`
     background-color: black;
-    flex-basis:30%;
-    max-height:100%;
+    height:100%;
+    width: 100%;
 `;
 
 /* style={{ textAnchor: 'middle', fontSize: '80%', fill: 'rgba(0, 0, 0, 0.87)' }} */
@@ -19,9 +19,10 @@ const UserPerformances = ({userPerformances}) => {
 
         <Wrapper>
             <ResponsiveContainer width="100%" height="100%">
-                    <RadarChart outerRadius={90} width={730} height={250} data={userPerformances}>
-                        <PolarGrid  stroke="#ffffff"  />
-                        <PolarAngleAxis dataKey="kind" style={{ fontSize: '80%', fill: 'rgba(255, 255, 255, 0.87)'}} />
+                    <RadarChart  outerRadius={100} width={730} height={250} data={userPerformances}>
+                        <PolarGrid stroke="white"  polarRadius={[0, 25, 50, 75, 100]} />
+                        
+                        <PolarAngleAxis tickLine={false} axisLine={false} dataKey="kind" style={{ fontSize: '80%', fill: 'rgba(255, 255, 255, 0.87)'}} />
                         <Radar name="" dataKey="value" fill="red" fillOpacity={0.6} />
                     </RadarChart>
             </ResponsiveContainer>
