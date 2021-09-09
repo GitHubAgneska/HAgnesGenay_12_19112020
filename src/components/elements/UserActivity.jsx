fae57dcea83d9f76e182fc99925bb6c279ad9bd5
Agneska <40915591+GitHubAgneska@users.noreply.github.com>
2021-09-09 08:10:24 +0200

    style : wrapping-up chart1 acivity (barchar) customization

--- src/components/elements/UserActivity.jsx
+++ src/components/elements/UserActivity.jsx
@@ -8,10 +8,18 @@ const Wrapper = styled.section`
     background-color:#74798C;
 `;
 
+const CustomTooltipClass = {
+    width:"50px",
+    height:"70px",
+    backgroundColor:"red",
+    color:"white", padding:"10%",
+    fontSize:"10px", fontWeight:"bold"
+} 
+
 const CustomTooltip = ({ active, payload }) => {
         if (active && payload && payload.length) {
         return (
-            <div style={{width:"50px",height:"100px", backgroundColor:"red", color:"white", padding:"10%"}}>
+            <div style={CustomTooltipClass}>
                 <p>{`${payload[0].value}`}Kg</p>
                 <p>{`${payload[1].value}`}kCal</p>
             </div>
@@ -19,22 +27,32 @@ const CustomTooltip = ({ active, payload }) => {
     }
     return null;
 };
+const CustomizedCursor = ({active}) => {
+    if (active) {
+        return ( <div style={{width:"50px",height:"100px",opacity:"0.5"}}></div> );
+    }
+    return null;
+}
+
+let getDay = (x) => { return (x.day.substr(x.day.length-1) > 0? x.day.substring(x.day.length-1):x.day.substring(x.day.length-2)); }
+let getAverageWeight = (x) => { let overAll = 0; let days = 0;  x.forEach(w => { overAll+=w; days+=1;return overAll/days; }) }
 
 const UserActivity = ({userActivitySessions}) => {
-    console.log(userActivitySessions);
+        
     return (
         <Wrapper>
             <ResponsiveContainer>
-                    <BarChart width={730} height={250} barSize={5} data={userActivitySessions}>
+                    <BarChart width={730} height={100} barSize={8} data={userActivitySessions}>
                         
-                        <XAxis />
-                        <YAxis orientation="right" tickCount="3" />
-                        {/*  <YAxis type= "category" allowDuplicatedCategory="true" dataKey="kilogram"  dataKey="calories" orientation="right" tickCount="3"  allowDecimals="false"/>*/}                        <ReferenceLine y={100} label="" stroke="grey" strokeDasharray="3 3" />
-                        <ReferenceLine y={250} stroke="grey" strokeDasharray="3 3" />
-                        <Legend/>
-                        <Bar dataKey="kilogram" fill="red"/>
-                        <Bar dataKey="calories" fill="black"/>
-                        <Tooltip content={<CustomTooltip />} cursor={false}/>
+                        <XAxis dataKey={getDay} tickLine={false} />
+                        
+                        <YAxis orientation="right" tickCount="4" allowDecimals={false}  tickLine={false} dataKey="kilogram"  domain={['dataMin +50', 'dataMax+3']}/>
+                        <ReferenceLine y="70" stroke="grey" strokeDasharray="3 3" />
+                        <Legend verticalAlign="top" height={36}/>
+                        <Bar dataKey="kilogram" fill="red" radius={[50, 50, 0, 0]} />
+                        <Bar dataKey="calories" fill="black" radius={[50, 50, 0, 0]} maxBarSize="60"/>
+                        <Tooltip content={<CustomTooltip />} />
+                        {/* <Tooltip content={<CustomTooltip />} cursor={<CustomizedCursor />}/> */}
                     </BarChart>
             </ResponsiveContainer>
         </Wrapper>
